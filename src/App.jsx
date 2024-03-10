d815f5d8c01301a54d7175b331b823ef89b23e96
garvitgabgab <108992700+garvitgabgab@users.noreply.github.com>
2024-03-10 22:07:58 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login/Login";
-import Dashboard from "./components/Dashboard/dashboard";
+import Dashboard from "../components/Dashboard";
 
 
 function App() {
